aa0443bb7df1bef372f96fced482245c6a1f837e
ScottWong <527407973@qq.com>
2020-11-03 01:06:09 +0800

    finish data process

--- idms_impl/data_process.py
+++ idms_impl/data_process.py
@@ -1,22 +1,27 @@
+from idms_impl.common.data_load import DataLoad
+from idms_impl.common.stopover_area import StopoverAreaSet
+
+
 class DataProcess:
     """数据处理
     将未处理的轨迹表转换成轨迹点为停留区域，并且具有多层语义的轨迹表
     """
-    def __init__(self, tr_dict):
+    def __init__(self):
         # 轨迹表
-        self.tr_dict = tr_dict
-        self.plist = self.gen_plist()
+        self.tr_dict = None
+        self.__stopover_area_set = None
+
+    def load_data(self, filename):
+        data_load = DataLoad()
+        self.tr_dict = data_load.load_data(filename="../../resource/test_1_user_with_flag.csv")
 
     def stopover_area_mining(self, eps, min_duration):
-        """停留区域点挖掘"""
-        for user_id, user in self.tr_dict.items():
-            pass
-        pass
+        # 停留区域挖掘
+        self.__stopover_area_set = StopoverAreaSet(self.tr_dict)
+        self.__stopover_area_set.stopover_area_mining(eps=eps, min_duration=min_duration)
+        self.tr_dict = self.__stopover_area_set.tr_dict
 
-    def semantic_tag_conversion(self, n, theta):
-        """语义标签转化"""
-        pass
-
-    def gen_plist(self):
-
-        pass
+    def semantic_tag_conversion(self, n, theta, knn_model, poi_list):
+        # 语义转化
+        self.__stopover_area_set.semantic_tag_conversion(n=n, theta=theta, knn_model=knn_model, poi_list=poi_list)
+        self.tr_dict = self.__stopover_area_set.tr_dict
--- idms_impl/test/test_data_process.py
+++ idms_impl/test/test_data_process.py
@@ -1,6 +1,7 @@
 from idms_impl.common.stopover_area import StopoverAreaSet
 from idms_impl.common.data_load import DataLoad, tr_dict_output
 from idms_impl.common.poi import POILoad
+from idms_impl.data_process import DataProcess
 
 
 def test_stopover_mining():
@@ -17,5 +18,19 @@ def test_stopover_mining():
     knn_model, poi_list = poi_load.knn_model, poi_load.poi_list
     stopover_area_set.semantic_tag_conversion(n=22, theta=0.99, knn_model=knn_model, poi_list=poi_list)
     tr_dict = stopover_area_set.tr_dict
+
+    tr_dict_output(tr_dict)
+
+
+def test_data_process():
+    print()
+    data_process = DataProcess()
+    data_process.load_data(filename="../../resource/test_1_user_with_flag.csv")
+    data_process.stopover_area_mining(eps=0.01, min_duration=5000)
+    poi_load = POILoad(filename="../../resource/nj_poi.csv")
+    poi_load.train_data(k=5)
+    knn_model, poi_list = poi_load.knn_model, poi_load.poi_list
+    data_process.semantic_tag_conversion(n=22, theta=0.99, knn_model=knn_model, poi_list=poi_list)
+    tr_dict = data_process.tr_dict
 
     tr_dict_output(tr_dict)
